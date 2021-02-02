deceb7c5b55eb172af4e256a2782b855fdc699a5
narek4241 <narekghazaryan02@gmail.com>
2021-02-02 05:01:35 +0400

    base setup components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Todo's</h1>
+      <Todos />
+      <AddTodo />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+  state = {};
+  render() {
+    return <div>Add Todo</div>;
+  }
+}
+
+export default AddTodo;
